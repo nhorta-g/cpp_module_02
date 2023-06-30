b85f0827cc73fd27d5f054ae1fb1f436f2214723
Nuno Grazina <nunogra@gmail.com>
2023-06-30 18:21:21 +0100

    ex00

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class FIXED_HPP {
+	public:
+	Fixed_HPP(void);
+	Fixed_HPP(const Fixed_HPP &rhs);
+	~Fixed_HPP(void);
+	Fixed_HPP &operator = (const Fixed_HPP &rhs);
+}
+
+#endif
+
+void function_name(int name) {
+	corpo;
+}
+
+void function_name(int name);
